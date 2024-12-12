5023fb244a62028a8fcbc4b9eac18c8f26ccd093
Nada Mohammed Soliman <153296755+Nada-Mohammd@users.noreply.github.com>
2024-12-12 14:09:10 +0200

    Update rbt.cpp

--- rbt.cpp
+++ rbt.cpp
@@ -170,132 +170,233 @@ private:
         return node;
     }
 
+//    void fixDelete(Node* x) {
+//        while (x != root && (x == nullptr || x->color == BLACK)) {
+//            //the node I want to delete in the left to the parent
+//            //so the brother in the right
+//            if (x == x->parent->left) {
+//                Node* sibling = x->parent->right;
+//                //if the brother is RED
+//                //rotation and recolor(parent,brother)
+//                if (sibling->color == RED) {
+//                    sibling->color = BLACK;
+//                    x->parent->color = RED;
+//                    leftRotate(x->parent);
+//                    sibling = x->parent->right;//update sibling after rotation
+//                }
+//                    /* Brother is Black */
+//                //if children of brother is black
+//                //pass the problem to the parent(will take DB)
+//                //if p is red->black
+//                //if p is black -> DB
+//                if ((sibling->left == nullptr || sibling->left->color == BLACK) &&
+//                    (sibling->right == nullptr || sibling->right->color == BLACK)) {
+//                    sibling->color = RED;
+//                    x = x->parent; // Propagate double black to parent
+//                } else {
+//                    //sibling's Right Child is Black, Left Child is Red
+//                    //(the near is Red)
+//                    //rotate and recolor(brother and child)
+//                    //perform the case of(far Red)
+//                    if (sibling->right == nullptr || sibling->right->color == BLACK) {
+//                        if (sibling->left) sibling->left->color = BLACK;
+//                        sibling->color = RED;
+//                        rightRotate(sibling);
+//                        sibling = x->parent->right;
+//                    }
+//                    //after handle the near is red -> handle the far is red
+//                    //rotate(parent,brother) , remove DB
+//                    //recolor: parent->black
+//                    // children of your brother->black
+//                    // brother-> original color of the parent
+//                    sibling->color = x->parent->color;
+//                    x->parent->color = BLACK;
+//                    if (sibling->right) sibling->right->color = BLACK;
+//                    leftRotate(x->parent);
+//                    x = root;
+//                }
+//                //x is right to the parent mirror the previous ):
+//            } else {
+//                Node* sibling = x->parent->left;
+//
+//                if (sibling->color == RED) {
+//                    sibling->color = BLACK;
+//                    x->parent->color = RED;
+//                    rightRotate(x->parent);
+//                    sibling = x->parent->left;
+//                }
+//
+//                if ((!sibling->left || sibling->left->color == BLACK) &&
+//                    (!sibling->right || sibling->right->color == BLACK)) {
+//                    sibling->color = RED;
+//                    x = x->parent;
+//                } else {
+//                    if (!sibling->left || sibling->left->color == BLACK) {
+//                        if (sibling->right) sibling->right->color = BLACK;
+//                        sibling->color = RED;
+//                        leftRotate(sibling);
+//                        sibling = x->parent->left;
+//                    }
+//
+//                    sibling->color = x->parent->color;
+//                    x->parent->color = BLACK;
+//                    if (sibling->left) sibling->left->color = BLACK;
+//                    rightRotate(x->parent);
+//                    x = root;
+//                }
+//            }
+//        }
+//
+//        if (x) x->color = BLACK;
+//    }
+//
+//
+//    Node* deleteBST(Node* root, int data) {
+//        //if the tree is empty
+//        //there is no thing to delete
+//        if (root == nullptr) return root;
+//        //search to find the node
+//        if (data < root->data) {
+//            root->left = deleteBST(root->left, data);
+//        } else if (data > root->data) {
+//            root->right = deleteBST(root->right, data);
+//        } else {
+//            //Case1:Node with no children
+//            if (root->left == nullptr and root->right == nullptr) {
+//                delete root;
+//                root = nullptr;
+//            }
+//            //Case2: Node with only one child
+//            else if (root->left == nullptr || root->right == nullptr)
+//            {
+//                Node* tmp;
+//                if(root->left != nullptr )
+//                {
+//                    tmp = root->left;
+//                }
+//                else
+//                {
+//                    tmp = root->right;
+//                }
+//                *root = *tmp; //copy the content of the child to root
+//                delete tmp; //delete the child(assume this case child is RED)
+//
+//            }
+//                //Case3: Node with two children
+//            else {
+//                //get the inorder successor(smallest in the right subtree)
+//                Node* temp = minValueNode(root->right);
+//                //copy the inorder successor's data to this node
+//                root->data = temp->data;
+//                //delete the inorder successor
+//                root->right = deleteBST(root->right, temp->data);
+//            }
+//        }
+//
+//        return root;
+//    }
     void fixDelete(Node* x) {
         while (x != root && (x == nullptr || x->color == BLACK)) {
-            //the node I want to delete in the left to the parent
-            //so the brother in the right
             if (x == x->parent->left) {
                 Node* sibling = x->parent->right;
-                //if the brother is RED
-                //rotation and recolor(parent,brother)
+
+                // Case 4: If DB's sibling is red
                 if (sibling->color == RED) {
-                    sibling->color = BLACK;
+                    sibling->color = BLACK; // Swap color of DB's parent with DB's sibling
                     x->parent->color = RED;
-                    leftRotate(x->parent);
-                    sibling = x->parent->right;//update sibling after rotation
+                    leftRotate(x->parent); // Perform rotation at parent node in the direction of DB node
+                    sibling = x->parent->right;
                 }
-                    /* Brother is Black */
-                //if children of brother is black
-                //pass the problem to the parent(will take DB)
-                //if p is red->black
-                //if p is black -> DB
+
+                // Case 3: If DB's sibling is black and its children are black
                 if ((sibling->left == nullptr || sibling->left->color == BLACK) &&
                     (sibling->right == nullptr || sibling->right->color == BLACK)) {
-                    sibling->color = RED;
-                    x = x->parent; // Propagate double black to parent
+                    sibling->color = RED; // Make DB's sibling red
+                    x = x->parent; // Move DB upwards
                 } else {
-                    //sibling's Right Child is Black, Left Child is Red
-                    //(the near is Red)
-                    //rotate and recolor(brother and child)
-                    //perform the case of(far Red)
+                    // Case 5: DB's sibling is black and its far child is black, near child is red
                     if (sibling->right == nullptr || sibling->right->color == BLACK) {
-                        if (sibling->left) sibling->left->color = BLACK;
+                        if (sibling->left) sibling->left->color = BLACK; // Swap color of sibling with its red child
                         sibling->color = RED;
-                        rightRotate(sibling);
+                        rightRotate(sibling); // Perform rotation at sibling node in the opposite direction of DB
                         sibling = x->parent->right;
                     }
-                    //after handle the near is red -> handle the far is red
-                    //rotate(parent,brother) , remove DB
-                    //recolor: parent->black
-                    // children of your brother->black
-                    // brother-> original color of the parent
-                    sibling->color = x->parent->color;
+
+                    // Case 6: DB's sibling is black and its far child is red
+                    sibling->color = x->parent->color; // Swap color of DB's parent with sibling's color
                     x->parent->color = BLACK;
-                    if (sibling->right) sibling->right->color = BLACK;
-                    leftRotate(x->parent);
-                    x = root;
+                    if (sibling->right) sibling->right->color = BLACK; // Change color of sibling's far red child to black
+                    leftRotate(x->parent); // Perform rotation at DB's parent in the direction of DB
+                    x = root; // Remove DB sign and make the node normal black node
                 }
-                //x is right to the parent mirror the previous ):
             } else {
                 Node* sibling = x->parent->left;
 
+                // Case 4: If DB's sibling is red
                 if (sibling->color == RED) {
-                    sibling->color = BLACK;
+                    sibling->color = BLACK; // Swap color of DB's parent with DB's sibling
                     x->parent->color = RED;
-                    rightRotate(x->parent);
+                    rightRotate(x->parent); // Perform rotation at parent node in the direction of DB node
                     sibling = x->parent->left;
                 }
 
-                if ((!sibling->left || sibling->left->color == BLACK) &&
-                    (!sibling->right || sibling->right->color == BLACK)) {
-                    sibling->color = RED;
-                    x = x->parent;
+                // Case 3: If DB's sibling is black and its children are black
+                if ((sibling->left == nullptr || sibling->left->color == BLACK) &&
+                    (sibling->right == nullptr || sibling->right->color == BLACK)) {
+                    sibling->color = RED; // Make DB's sibling red
+                    x = x->parent; // Move DB upwards
                 } else {
-                    if (!sibling->left || sibling->left->color == BLACK) {
-                        if (sibling->right) sibling->right->color = BLACK;
+                    // Case 5: DB's sibling is black and its far child is black, near child is red
+                    if (sibling->left == nullptr || sibling->left->color == BLACK) {
+                        if (sibling->right) sibling->right->color = BLACK; // Swap color of sibling with its red child
                         sibling->color = RED;
-                        leftRotate(sibling);
+                        leftRotate(sibling); // Perform rotation at sibling node in the opposite direction of DB
                         sibling = x->parent->left;
                     }
 
-                    sibling->color = x->parent->color;
+                    // Case 6: DB's sibling is black and its far child is red
+                    sibling->color = x->parent->color; // Swap color of DB's parent with sibling's color
                     x->parent->color = BLACK;
-                    if (sibling->left) sibling->left->color = BLACK;
-                    rightRotate(x->parent);
-                    x = root;
+                    if (sibling->left) sibling->left->color = BLACK; // Change color of sibling's far red child to black
+                    rightRotate(x->parent); // Perform rotation at DB's parent in the direction of DB
+                    x = root; // Remove DB sign and make the node normal black node
                 }
             }
         }
 
-        if (x) x->color = BLACK;
+        if (x) x->color = BLACK; // Ensure DB is removed and root becomes black if necessary
     }
 
-
     Node* deleteBST(Node* root, int data) {
-        //if the tree is empty
-        //there is no thing to delete
         if (root == nullptr) return root;
-        //search to find the node
+
         if (data < root->data) {
             root->left = deleteBST(root->left, data);
         } else if (data > root->data) {
             root->right = deleteBST(root->right, data);
         } else {
-            //Case1:Node with no children
-            if (root->left == nullptr and root->right == nullptr) {
-                delete root;
-                root = nullptr;
-            }
-            //Case2: Node with only one child
-            else if (root->left == nullptr || root->right == nullptr)
-            {
-                Node* tmp;
-                if(root->left != nullptr )
-                {
-                    tmp = root->left;
-                }
-                else
-                {
-                    tmp = root->right;
-                }
-                *root = *tmp; //copy the content of the child to root
-                delete tmp; //delete the child(assume this case child is RED)
+            // Case 1: Node to be deleted is a red leaf node
+            if (root->left == nullptr || root->right == nullptr) {
+                Node* temp = root->left ? root->left : root->right;
 
-            }
-                //Case3: Node with two children
-            else {
-                //get the inorder successor(smallest in the right subtree)
+                if (temp == nullptr) {
+                    temp = root;
+                    root = nullptr; // If null DB, delete the node
+                } else {
+                    *root = *temp; // Copy the content of the child node
+                }
+
+                delete temp;
+            } else {
+                // Node with two children, use inorder successor (minimum value in the right subtree)
                 Node* temp = minValueNode(root->right);
-                //copy the inorder successor's data to this node
-                root->data = temp->data;
-                //delete the inorder successor
-                root->right = deleteBST(root->right, temp->data);
+                root->data = temp->data; // Replace data with inorder successor's data
+                root->right = deleteBST(root->right, temp->data); // Delete the successor
             }
         }
 
         return root;
     }
-
 
 public:
     RedBlackTree() : root(nullptr) {}
